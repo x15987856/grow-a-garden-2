4c72f7bc0f7c035af1aea9c18c679219c9abbb7d
x15987856 <x15987856@gmail.com>
2026-06-22 23:12:25 +0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true
+  output: 'export', // 强行开启纯静态导出，不走服务器解析
+  images: {
+    unoptimized: true, // 静态模式下必须关闭图片优化，否则打包报错
+  },
 };
 
 export default nextConfig;
